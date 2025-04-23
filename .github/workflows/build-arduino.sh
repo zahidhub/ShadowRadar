1677e85f19c89e84db744521fd8b2356902ef769
Hasan Zahid <guszahidha@student.gu.se>
2025-04-23 04:23:49 +0200

    #6 fix bugs in the pipeline

--- .github/workflows/build-arduino.sh
+++ .github/workflows/build-arduino.sh
@@ -3,30 +3,41 @@
 
 #inspiration: https://medium.com/swlh/how-to-create-an-automated-build-pipeline-for-your-arduino-project-1df9826f2a5e
 
+#!/bin/bash
+set -e
+shopt -s globstar
+
 MODE=$1
+
 if [ "$MODE" == "build-only" ]; then
-  # Run build logic
   cd $GITHUB_WORKSPACE
+
   mkdir -p $HOME/Arduino
   mkdir -p $HOME/Arduino/libraries
+
+  # Install Arduino CLI and AVR core
   export PATH=$PATH:$GITHUB_WORKSPACE/bin
   curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.sh | sh
-  arduino-cli config init
+  arduino-cli config init || true  # Ignore if config exists
   arduino-cli core update-index
   arduino-cli core install arduino:avr
   arduino-cli lib install AUnit
-  ln -s $GITHUB_WORKSPACE $HOME/Arduino/libraries/CI_Test_Library
+
+  # Compile main project
   mkdir Radar
   mv src/Radar.ino Radar/Radar.ino
   arduino-cli compile --fqbn arduino:avr:uno Radar
+
 elif [ "$MODE" == "test-only" ]; then
-  # Run tests
+  # Host-based test only: Skip Arduino CLI setup
   cd $GITHUB_WORKSPACE/tests
   make run
+
 else
   echo "Invalid mode. Use 'build-only' or 'test-only'."
   exit 1
 fi
+
 
 
 #!/bin/bash
